d897ed92e44046030e994909473548b48873f477
Anne <Anne@macbook-air-van-anne.home>
2026-04-08 20:52:40 +0200

    fix: scripts werkend maken

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,11 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  // Verander 'studiebuddy' naar de naam van jouw GitHub-repo
-  base: '/studiebuddy/',
+  // Verander 'studybuddy' naar de naam van jouw GitHub-repo
+  base: '/studybuddy/',
+  define: {
+    __SUPABASE_URL__: JSON.stringify(process.env.VITE_SUPABASE_URL),
+    __SUPABASE_KEY__: JSON.stringify(process.env.VITE_SUPABASE_KEY),
+    __ANTHROPIC_KEY__: JSON.stringify(process.env.VITE_ANTHROPIC_KEY),
+  }
 })
